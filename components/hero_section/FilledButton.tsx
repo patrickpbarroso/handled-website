a3bac98a93d3ed57dd400b30c304611bd22fafed
Patrick Barroso <patrick.plbarroso@gmail.com>
2025-09-29 21:00:43 -0300

    fix(.tsx): Adjust text size inside buttons

--- components/hero_section/FilledButton.tsx
+++ components/hero_section/FilledButton.tsx
@@ -1,6 +1,6 @@
 const FilledButton = ({ content }: {content: string}) => {
     return (
-        <button className="w-[120px] py-[5px] bg-[#3A58BA] text-[#EEEEF8] rounded-[25px] text-lg">
+        <button className="w-[120px] py-[5px] bg-[#3A58BA] text-[#EEEEF8] rounded-[25px] text-[16px]">
             {content}
         </button>
     )
--- components/hero_section/HollowButton.tsx
+++ components/hero_section/HollowButton.tsx
@@ -1,6 +1,6 @@
 const HollowButton = ({ content }: {content: string}) => {
     return (
-        <button className="px-[20px] border border-[#BFBCCC] text-[#EEEEF8] rounded-[25px] text-lg">
+        <button className="px-[20px] border border-[#BFBCCC] text-[#EEEEF8] rounded-[25px] text-[16px]">
             {content}
         </button>
     )
